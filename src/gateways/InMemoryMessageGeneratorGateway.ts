813a566eb89e2501bf34c2cbdb90a62c04997435
Satoshi Kawabata <kwst.andromeda.ma@gmail.com>
2024-02-02 23:57:47 +0900

    人間に他する反論を実装

--- src/gateways/InMemoryMessageGeneratorGateway.ts
+++ src/gateways/InMemoryMessageGeneratorGateway.ts
@@ -40,16 +40,14 @@ ${joinedMessage}`,
         role: "system",
       },
       {
-        content: createAiPrompt(info.userName, info.aiMessageContent),
-        role: "assistant",
+        content: createAiPrompt(
+          info.userName,
+          info.aiMessageContent,
+          info.humanMessageContent
+        ),
+        role: "user",
       },
     ];
-    if (info.humanMessageContent) {
-      messages.push({
-        content: info.humanMessageContent,
-        role: "user",
-      });
-    }
     const txt = await createChatCompletion(apiKey, {
       messages,
     });
@@ -65,8 +63,34 @@ ${joinedMessage}`,
   }
 }
 
-const createAiPrompt = (userName: string, content: string) => {
-  return `
+const createAiPrompt = (
+  userName: string,
+  content: string,
+  humanContent?: string
+) => {
+  return humanContent
+    ? `
+    ${userName}さんが
+    「${content}」と言っています。
+    人間が「${humanContent}」と言ってます。
+    ${userName}さんの意見をふまえて人間に対する反論を書いてください。
+    その際に以下のルールを守ってください。
+    ・160文字程度で書いてください。
+    ・自分のエゴを全面に押し出してください。
+    ・簡潔に反論だけを述べてください。
+    ・自然な会話の流れを保ってください。
+    ・口語口調で書いてください。
+    ・相手の論理の虚を突いてください。
+    ・相手の揚げ足を取ってください。
+    
+    必ず以下のJSONフォーマットでtargetとcontentという変数名を変えずに返答ください。
+    {
+      "target": "人間",
+      "content": "{あなたの反論}"
+    }
+    
+`
+    : `
 ${userName}さんが
 「${content}」と言っています。
 それに対する反論を書いてください。
@@ -85,5 +109,17 @@ ${userName}さんが
   "content": "{あなたの反論}"
 }
 
-    `;
+`;
 };
+
+/*
+
+人間の後の3発言くらいは人間の発言を加味する
+
+人間の発言のときにparentMessageIdの発言の何割再生したかを一緒に送ってもらう
+- 50%未満なら切る → その場合はparentMessageIdの発言はなかったことになる
+- それか、%分だけ削るか → その場合はparentMessageIdの発言の途中までになる
+
+APIドキュメントを更新する
+
+*/
